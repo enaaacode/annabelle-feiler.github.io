9780ab207d2f3fa136b8ecacfde71e06fc44ed57
enaaacode <152639159+enaaacode@users.noreply.github.com>
2025-06-22 13:18:20 +0200

    Create mobile_overlay.js

---
+++ assets/js/mobile_overlay.js
@@ -0,0 +1,29 @@
+// HINWEIS
+// Wird nicht genutzt -> stattdessen focustrap.js
+
+const burgerIcon = document.getElementById('burger-icon');
+const closeIcon = document.getElementById('close-button');
+const mobileOverlay = document.getElementById('mobile-overlay');
+
+// Menü öffnen
+burgerIcon.addEventListener('click', function () {
+  mobileOverlay.style.display = 'flex';
+  burgerIcon.style.display = 'none';
+});
+
+// Menü schließen
+closeIcon.addEventListener('click', function () {
+  mobileOverlay.style.display = 'none';
+  burgerIcon.style.display = 'flex';
+});
+
+// Alle Links im Overlay holen
+const overlayLinks = mobileOverlay.querySelectorAll('a');
+
+// Wenn ein Link im Overlay angeklickt wird → Menü schließen
+overlayLinks.forEach(function (link) {
+  link.addEventListener('click', function () {
+    mobileOverlay.style.display = 'none';
+    burgerIcon.style.display = 'flex';
+  });
+});
